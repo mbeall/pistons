b26253f98f51b0b37717509efa91db7d53e923ec
Matt Beall <mbeall@starverte.com>
2015-01-28 20:56:57 -0700

    Register secondary menu

--- functions.php
+++ functions.php
@@ -2,7 +2,14 @@
 /**
  * Pistons functions and definitions
  *
- * @package Flint/Pistons
+ * @package Flint\Pistons
  * @since Pistons 0.0.0
  */
 
+add_filter('flint_nav_menus', 'pistons_nav_menus');
+function pistons_nav_menus( $nav_menus ) {
+  $pistons_nav_menus = array(
+    'branding' => __( 'Header Menu', 'flint' ),
+  );
+  return array_merge( $nav_menus, $pistons_nav_menus );
+}
