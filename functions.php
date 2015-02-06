201ecd6e3b52030668806193f7abd86c4dd2dc41
Matt Beall <mbeall@starverte.com>
2015-02-05 17:37:41 -0700

    Add image sizes and front page slider option

--- functions.php
+++ functions.php
@@ -6,10 +6,25 @@
  * @since Pistons 0.0.0
  */
 
-add_filter('flint_nav_menus', 'pistons_nav_menus');
 function pistons_nav_menus( $nav_menus ) {
   $pistons_nav_menus = array(
     'branding' => __( 'Header Menu', 'flint' ),
   );
   return array_merge( $nav_menus, $pistons_nav_menus );
 }
+add_filter('flint_nav_menus', 'pistons_nav_menus');
+
+function pistons_after_setup_theme() {
+
+  require( get_stylesheet_directory() . '/inc/customizer.php' );
+
+  require( get_stylesheet_directory() . '/inc/options.php' );
+
+  add_image_size( 'product-view'   ,  170, 170, true );
+  add_image_size( 'product-image'  ,  270, 210, true );
+  add_image_size( 'category-image' ,  270, 270, true );
+  add_image_size( 'featured-slider', 1170, 360, true );
+
+  set_post_thumbnail_size( 770, 500, true );
+}
+add_action( 'after_setup_theme', 'pistons_after_setup_theme', 20 );
