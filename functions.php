bd5cdb4b71074c371b61066e6b3a58d979e1ea95
Matt Beall <mbeall@starverte.com>
2015-02-14 15:12:06 -0700

    Add new image size instead of defining thumb size

--- functions.php
+++ functions.php
@@ -15,16 +15,14 @@ function pistons_nav_menus( $nav_menus ) {
 add_filter('flint_nav_menus', 'pistons_nav_menus');
 
 function pistons_after_setup_theme() {
-
   require( get_stylesheet_directory() . '/inc/customizer.php' );
 
   require( get_stylesheet_directory() . '/inc/options.php' );
 
-  add_image_size( 'product-view'   ,  170, 170, true );
-  add_image_size( 'product-image'  ,  270, 210, true );
-  add_image_size( 'category-image' ,  270, 270, true );
-  add_image_size( 'featured-slider', 1170, 360, true );
-
-  set_post_thumbnail_size( 770, 500, true );
+  add_image_size( 'product-view'    ,  170, 170, true );
+  add_image_size( 'product-image'   ,  270, 210, true );
+  add_image_size( 'category-image'  ,  270, 270, true );
+  add_image_size( 'product-featured',  770, 500, true );
+  add_image_size( 'featured-slider' , 1170, 360, true );
 }
 add_action( 'after_setup_theme', 'pistons_after_setup_theme', 20 );
